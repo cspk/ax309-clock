4f82371a6a38abc9e5276ecb5837bcc4b8536002
Pavel Serebryakov <serebryakovp@progressufa.ru>
2018-01-26 12:02:47 +0500

    clock: stub implementation

--- clock.v
+++ clock.v
@@ -1,10 +1,16 @@
 `timescale 1ns / 1ps
 
 module clock(
-	input wire clk
+	input wire clk,
+
+	output wire[7:0] digit_segs,
+	output wire[5:0] position
 );
 
-always @ (posedge clk) begin
-end
+wire[2:0] digit_pos;
+wire[4:0] digit, digit_cur;
+
+display disp(digit, latch, clk, digit_pos, digit_cur);
+sseg s(digit_cur, digit_pos, digit_segs, position);
 
 endmodule
