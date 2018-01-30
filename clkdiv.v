3037a548d99302d1113497af5d2037f54fc73c5a
Pavel Serebryakov <serebryakovp@progressufa.ru>
2018-01-30 09:46:26 +0500

    Add clkdiv module

---
+++ clkdiv.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+module clkdiv(
+	input wire clk,
+	input wire[5:0] divisor, // logarithm base 2
+	
+	output reg divclk
+);
+
+reg[31:0] cnt = 31'd0;
+
+always @ (posedge clk) begin
+	cnt <= cnt + 1;
+end
+
+always @ (posedge cnt[divisor]) begin
+	divclk <= ~divclk;
+end
+
+endmodule
