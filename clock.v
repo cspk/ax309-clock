8314ae3404a98822301f9611318b9dcdf6241306
Pavel Serebryakov <serebryakovp@progressufa.ru>
2018-02-01 16:00:17 +0500

    rtc: add IOs

--- clock.v
+++ clock.v
@@ -3,13 +3,19 @@ module clock(
 	input nrst,
 
 	output reg[7:0] digit_segs,
-	output reg[5:0] digit_sel
+	output reg[5:0] digit_sel,
+
+	output reg rtc_sclk,
+	output reg rtc_ce,
+	inout rtc_data_io
 );
 
 always @ (posedge clk or negedge nrst) begin
 	if (!nrst) begin
 		digit_segs <= 8'd0;
 		digit_sel <= 8'd0;
+		rtc_sclk <= 1'd0;
+		rtc_ce <= 1'd0;
 	end
 end
 
--- rtc.v
+++ rtc.v
@@ -1,5 +1,10 @@
 module rtc(
+	input sclk,
+	input poll_freq,
 
+	output reg ce,
+
+	inout data_io
 );
 
 
